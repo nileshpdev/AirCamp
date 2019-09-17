be74a209199c51dfabbd252430d0c06d53bb4beb
Nilesh <nile2h@gmail.com>
2019-09-17 22:20:02 +0000

    changed port to 5000

--- app.js
+++ app.js
@@ -59,4 +59,4 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP);
+app..listen(process.env.PORT || 5000)
