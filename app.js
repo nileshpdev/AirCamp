cbbf8e90f19122857c75cea79dd0865831a46c30
Nilesh <nile2h@gmail.com>
2019-09-17 22:12:16 +0000

    updated app.listen

--- app.js
+++ app.js
@@ -59,6 +59,4 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
-	console.log('server listening on 3000');
-});
+app.listen(process.env.PORT, process.env.IP);
