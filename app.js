db8098b877d7173b22628886bfc2d93454740ed1
Nilesh <nile2h@gmail.com>
2019-09-17 23:05:03 +0000

    5000

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
-	console.log("The AirCamp Server Has Started")
+app.listen(5000, () => {
+	console.log('server listening on 3000');
 });
