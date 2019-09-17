23769156be662819e9b7590e98da968699defc26
Nilesh <nile2h@gmail.com>
2019-09-17 23:44:53 +0000

    app.listen

--- app.js
+++ app.js
@@ -59,4 +59,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.set('port', process.env.PORT || 8080);
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
