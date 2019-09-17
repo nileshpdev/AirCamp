37ce25d65c70a9796893f49ffca103fa477d05f6
Nilesh <nile2h@gmail.com>
2019-09-17 22:56:06 +0000

    updated app.js

--- app.js
+++ app.js
@@ -59,4 +59,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app..listen(process.env.PORT || 5000)
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("The AirCamp Server Has Started")
+});
